481f4d398021f50e4d18e90bb4ec3aa85841a9f6
Emerson Arevalo <AO22002@ues.edu.sv>
2024-10-22 15:14:44 -0600

    Especificando la ruta relativa para buscar los templates

--- SistemaSIC/settings.py
+++ SistemaSIC/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'SistemaSIC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'contable/templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
