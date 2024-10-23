61f1720dd91f1f0d175cfb74440f6189262d7886
Emerson Arevalo <AO22002@ues.edu.sv>
2024-10-22 20:25:55 -0600

    Realizando migraciones de la BD

--- contable/admin.py
+++ contable/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PeriodoContable, EstadoFinanciero, CuentaContable, AsientoContable, Transaccion, TransaccionCuenta
 
 # Register your models here.
+admin.site.register(PeriodoContable)
+admin.site.register(EstadoFinanciero)
+admin.site.register(CuentaContable)
+admin.site.register(AsientoContable)
+admin.site.register(Transaccion)
+admin.site.register(TransaccionCuenta)
